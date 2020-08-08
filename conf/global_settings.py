4ebed26482438054f92b9347bdf3949fcd7c6121
Doan-Nguyen <doan.nguyenngoc950@gmail.com>
2020-08-09 01:36:38 +0700

    upl: colab path

--- conf/global_settings.py
+++ conf/global_settings.py
@@ -11,8 +11,8 @@ CHECKPOINT_PATH = './checkpoint'
 DATASET_FOLDER_LAP = "/media/doannn/data/Projects/Works/SealProject/Datasets/data_from_internet/cifar100_imgs"
 DATASET_FOLDER_COLAB = "/content/drive/My Drive/Data_Backup/Image_classification/Datasets/From_internet/cifar100_imgs"
 
-TRAIN_FOLDER = os.path.join(DATASET_FOLDER_LAP, "train")
-TEST_FOLDER = TRAIN_FOLDER = os.path.join(DATASET_FOLDER_LAP, "test")
+TRAIN_FOLDER = os.path.join(DATASET_FOLDER_COLAB, "train")
+TEST_FOLDER = TRAIN_FOLDER = os.path.join(DATASET_FOLDER_COLAB, "test")
 
 # STANDARD_FOLDER = 
 
