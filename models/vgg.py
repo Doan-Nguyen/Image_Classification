da0005f7f050a5f124742b3970c6bf543735fe7d
Doan-Nguyen <doan.nguyenngoc950@gmail.com>
2020-08-09 02:19:19 +0700

    fix: change input_feature

--- models/vgg.py
+++ models/vgg.py
@@ -49,11 +49,12 @@ class VGG(nn.Module):
 
     def forward(self, x):
         print("X: {}".format(x.shape))
-        x = x.view(x.size(0), -1)
+        # x = x.view(x.size(0), -1)
         output = self.features(x) # [batch_size, ]
         print("Output 1: {} ".format(output.shape))
         output = output.view(output.size()[0], -1)  # ()
         print("Output 2: {} ".format(output.shape))
+        # x = x.view(output.size(0), -1)
         output = self.classifier(output)
     
         return output
